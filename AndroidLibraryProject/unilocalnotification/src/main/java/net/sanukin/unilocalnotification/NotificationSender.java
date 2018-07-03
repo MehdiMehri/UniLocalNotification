40e392d74cbb6dba32fcee91715efa778c97a48f
Mehdi Mehri <Mehdi.2m712@gmail.com>
2018-07-03 10:09:27 +0430

    try to Open channel on andorid O
    Try to add android O local notification
    
    Co-Authored-By: mehdimehri <mehdimehri@users.noreply.github.com>

--- AndroidLibraryProject/unilocalnotification/src/main/java/net/sanukin/unilocalnotification/NotificationSender.java
+++ AndroidLibraryProject/unilocalnotification/src/main/java/net/sanukin/unilocalnotification/NotificationSender.java
@@ -7,8 +7,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationManagerCompat;
 
-import java.util.Calendar;
+// add for android O notification
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+//======================================
 
+import java.util.Calendar;
 import com.unity3d.player.UnityPlayer;
 
 /**
@@ -16,6 +25,77 @@ import com.unity3d.player.UnityPlayer;
  */
 
 public class NotificationSender {
+    private boolean isInitilized = false;
+    private NotificationManager mManager;
+    public static final String ANDROID_CHANNEL_ID = "com.chikeandroid.tutsplustalerts.ANDROID";
+    public static final String IOS_CHANNEL_ID = "com.chikeandroid.tutsplustalerts.IOS";
+    public static final String ANDROID_CHANNEL_NAME = "ANDROID CHANNEL";
+    public static final String IOS_CHANNEL_NAME = "IOS CHANNEL";
+
+    public static void initilizeAndroid8(string android_chanel_ID, String ios_chanel_ID){
+        ANDROID_CHANNEL_ID = android_chanel_ID;
+        IOS_CHANNEL_ID = ios_chanel_ID;
+        // create android channel
+        NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID, ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+        // Sets whether notifications posted to this channel should display notification lights
+        androidChannel.enableLights(true);
+        // Sets whether notification posted to this channel should vibrate.
+        androidChannel.enableVibration(true);
+        // Sets the notification light color for notifications posted to this channel
+        androidChannel.setLightColor(Color.GREEN);
+        // Sets whether notifications posted to this channel appear on the lockscreen or not
+        androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(androidChannel);
+
+        // create ios channel
+        NotificationChannel iosChannel = new NotificationChannel(IOS_CHANNEL_ID, IOS_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+        iosChannel.enableLights(true);
+        iosChannel.enableVibration(true);
+        iosChannel.setLightColor(Color.GRAY);
+        iosChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        getManager().createNotificationChannel(iosChannel);
+
+        isInitilized = true;
+    }
+
+    private NotificationManager getManager() {
+        if (mManager == null) {
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return mManager;
+    }
+
+    // ...
+    public Notification.Builder getAndroidChannelNotification(String title, String body) {
+        return new Notification.Builder(getApplicationContext(), ANDROID_CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(android.R.drawable.stat_notify_more)
+                .setAutoCancel(true);
+    }
+
+    public Notification.Builder getIosChannelNotification(String title, String body) {
+        return new Notification.Builder(getApplicationContext(), IOS_CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(android.R.drawable.stat_notify_more)
+                .setAutoCancel(true);
+    }
+
+    public static void sendAndroid8Notification(string title, string text, int delay, ){
+        if(!isInitilized){
+
+            this.initilizeAndroid8("","");
+
+        }
+
+        Notification.Builder nb = getAndroidChannelNotification(title, text);
+
+        getManager().notify(101, nb.build());
+
+    }
+
     /**
      * Set local notification
      * @param title notification title
